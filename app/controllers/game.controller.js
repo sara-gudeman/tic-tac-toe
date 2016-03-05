61b3f918b821e2ee9190204fe96f1e9dae87369e
Sara Gudeman <sgudeman982@gmail.com>
2016-03-05 13:58:44 -0800

    (feat) rough draft for gameboard rendering

--- app/controllers/game.controller.js
+++ app/controllers/game.controller.js
@@ -5,6 +5,21 @@ angular
 GameController.$inject = ['$scope'];
 
 function GameController ($scope) {
-  console.log('hello');
   $scope.hello = 'hello';
+
+  function makeBoard (n) {
+    var board = [];
+
+    for (var i = 0; i < n; i++) {
+      var row = new Array(n);
+      board.push(row);
+    }
+
+    return board;
+  }
+
+  var board = makeBoard(3);
+
+  $scope.board = board;
+  
 }
