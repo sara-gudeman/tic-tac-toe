8e1d070f4c08e7dad5c894af397814b0bc43adbf
Sara Gudeman <sgudeman982@gmail.com>
2016-03-05 15:53:57 -0800

    (feat) game play toggles between players

--- app/controllers/game.controller.js
+++ app/controllers/game.controller.js
@@ -17,8 +17,21 @@ function GameController ($scope) {
     }
   ];
 
-  $scope.currentPlayer = $scope.players[0];
-  $currentPiece = $scope.currentPlayer.gamePiece;
+  $scope.playerId = 0;
+
+  $scope.currentPlayer = $scope.players[$scope.playerId];
+  $scope.currentPiece = $scope.currentPlayer.gamePiece;
+
+  // later this can be developed such that multiple independent games can be played at once
+  // this function currently only allows for two players
+  function changePlayers (players, playerId) {
+    var updateId = playerId === 0 ? 1 : 0;
+    return updateId;
+  }
+  
+  // later this can be developed such that multiple independent games can be played at once
+  // this function currently only allows for two players
+  $scope.changePlayers = changePlayers.bind(null, $scope.players);
 
   // BOARD FUNCTIONALITY
   function makeBoard (n) {
@@ -36,12 +49,23 @@ function GameController ($scope) {
 
   $scope.board = board;
 
-  // GAME FUNCTIONALITY
+  // GAME PIECE FUNCTIONALITY
   function togglePiece (row, col) {
-    console.log(row, col);
-    $scope.board[row][col] = $currentPiece;
+    console.log(row, col, $scope.currentPiece);
+    $scope.board[row][col] = $scope.currentPiece;
   }
 
   $scope.togglePiece = togglePiece;
+
+  // GAME LOGIC FUNCTIONALITY
+  // TODO: REMOVE $SCOPE REFERENCES SO THAT THIS CAN BE PULLED OUT INTO SERVICE
+  function updateGame (row, col, playerId) {
+    $scope.togglePiece(row, col);
+    $scope.playerId = $scope.changePlayers(playerId);
+    $scope.currentPlayer = $scope.players[$scope.playerId];
+    $scope.currentPiece = $scope.currentPlayer.gamePiece;
+  }
+
+  $scope.updateGame = updateGame;
   
 }
